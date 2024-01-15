83e4c47cde21d2dbe1ea93c485654a9882eaa513
Ximo Llacer <xllaber7@gmail.com>
2024-01-15 19:37:17 +0100

    renamed files to follow flyway names

--- src/main/resources/static/db/V202401151911__createDatabaseAndTables.sql
+++ src/main/resources/static/db/V202401151911__createDatabaseAndTables.sql
@@ -1,14 +1,14 @@
 drop database if exists book_archive;
 create database book_archive;
 use book_archive;
-create table saga (
+create table if not exists saga (
     id bigint auto_increment primary key,
     name varchar(100),
     publish_start int not null,
     publish_end int
 );
 
-create table authors (
+create table if not exists authors (
     id bigint auto_increment primary key,
     name varchar(100) not null,
     country varchar(100),
@@ -17,7 +17,7 @@ create table authors (
     image varchar(100)
 );
 
-create table authors_books (
+create table if not exists authors_books (
     id bigint auto_increment primary key,
     pseudonym varchar(100),
     author_id bigint not null,
@@ -28,14 +28,14 @@ alter table authors_books add constraint authors_books__authors___fk foreign key
 alter table authors_books add constraint authors_books__books___fk foreign key (book_id) references books(id)
     on delete cascade;
 
-create table rereads (
+create table if not exists rereads (
     id bigint auto_increment primary key,
     impression varchar(5000),
     start_date date,
     finish_date date,
     book_id bigint not null
 );
-alter table rereads add constraint rereads_book_fk foreign key (book_id) references books(id)
+alter table if not exists rereads add constraint rereads_book_fk foreign key (book_id) references books(id)
     on delete cascade;
 
 create table genres (
@@ -43,7 +43,7 @@ create table genres (
     name varchar(100)
 );
 
-create table books_genres (
+create table if not exists books_genres (
     id bigint auto_increment primary key,
     book_id bigint not null,
     genre_id bigint not null
@@ -53,7 +53,7 @@ alter table books_genres add constraint books_genres__genres___fk foreign key (g
 alter table books_genres add constraint books_genres__books___fk foreign key (book_id) references books(id)
     on delete cascade;
 
-create table books (
+create table if not exists books (
     id bigint auto_increment primary key,
     title varchar(100) not null,
     pages int not null,
