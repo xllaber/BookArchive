b8785eaa970205a4fcd01120f97d673c7a45b6df
Ximo Llacer <xllaber7@gmail.com>
2024-05-11 11:42:04 +0200

    feat(#16): Created insert book dialog

--- web/src/app/components/books/insert-form-dialog/insert-form-dialog.component.ts
+++ web/src/app/components/books/insert-form-dialog/insert-form-dialog.component.ts
@@ -15,8 +15,8 @@ import {MatCheckbox} from "@angular/material/checkbox";
 import {MAT_RADIO_DEFAULT_OPTIONS, MatRadioButton, MatRadioGroup} from "@angular/material/radio";
 import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
 import {MAT_DATE_LOCALE} from "@angular/material/core";
-import {MultimediaService} from "../../../services/multimedia.service";
-import {File} from "buffer";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {SnackbarComponent} from "../../../shared/snackbar/snackbar.component";
 
 @Component({
   	selector: 'app-insert-form-dialog',
@@ -61,9 +61,11 @@ export class InsertFormDialogComponent implements OnInit{
 		pages: 0,
 		publishYear: 0,
 		fave: false,
-		reread: null,
 		authors: [],
-		genre: []
+		genre: [],
+		rereadCreateRequest: null,
+		authorIds: [],
+		genreIds: []
 	};
 	sagaOptions!: Saga[];
 	authorOptions!: Author[];
@@ -75,7 +77,7 @@ export class InsertFormDialogComponent implements OnInit{
 				private sagaService: SagaService,
 				private authorService: AuthorService,
 				private genreService: GenreService,
-				private multimediaService: MultimediaService) {
+				private snackBar: MatSnackBar) {
 		this.form = this.formBuilder.group({
 			title: ["", Validators.required],
 			saga: [""],
@@ -112,16 +114,30 @@ export class InsertFormDialogComponent implements OnInit{
 	save() {
 		console.log(this.form.value);
 		if(this.form.valid) {
-			this.book.title = this.form.controls['title'].value;
-			this.book.saga = this.form.controls['saga'].value;
+			this.book.sagaId = this.form.controls['saga'].value;
 			this.book.sagaNum = this.form.controls['sagaNum'].value;
-			this.book.authors = this.form.controls['authors'].value;
+			this.book.authorIds = this.form.controls['authors'].value;
 			this.book.publishYear = this.form.controls['publishYear'].value;
 			this.book.pages = this.form.controls['pages'].value;
 			this.book.fave = this.form.controls['fave'].value;
-			this.book.genre = this.form.controls['genres'].value;
-			this.book.reread = this.form.controls['reread'].value;
-			// this.bookService.insert(this.book);
+			this.book.genreIds = this.form.controls['genres'].value;
+			this.book.rereadCreateRequest = this.form.controls['reread'].value;
+			this.bookService.insert(this.book).subscribe(
+				(data) => this.snackBar.openFromComponent(SnackbarComponent, {
+					data: {
+						message: 'El libro se ha insertado correctamente',
+						success: true
+					},
+					duration: 4000
+				}),
+				(error) => this.snackBar.openFromComponent(SnackbarComponent, {
+					data: {
+						message: `Error ${error.error.code}: ${error.error.message}`,
+						success: false
+					},
+					duration: 4000
+				})
+			);
 			// this.multimediaService.upload(this.coverUpload);
 		}
 		console.log(this.book);
--- web/src/app/services/book.service.ts
+++ web/src/app/services/book.service.ts
@@ -24,8 +24,8 @@ export class BookService {
 	return this.http.get<Book>(`${this.url}/${id}`);
   }
 
-  insert(book: Book) {
-
+  insert(book: Book): Observable<Book> {
+	return this.http.post<Book>(`${this.url}`, book);
   }
 
   update(book: Book) {
